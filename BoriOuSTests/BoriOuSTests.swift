b08bd556fd608e4810d1d64474e70ad01ca07fb7
Elouann Domenech <elouann.dom@gmail.com>
2026-01-10 09:31:48 -0500

    Initial Commit

---
+++ BoriOuSTests/BoriOuSTests.swift
@@ -0,0 +1,17 @@
+//
+//  BoriOuSTests.swift
+//  BoriOuSTests
+//
+//  Created by Elouann Domenech on 2026-01-10.
+//
+
+import Testing
+@testable import BoriOuS
+
+struct BoriOuSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
